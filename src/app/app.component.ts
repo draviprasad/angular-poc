e89a51756ae396f48603923bbd3986d512038820
rdammu <rdammu@evoketechnologies.com>
2021-04-28 18:28:08 +0530

    xml2js added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+// import * as js2xmlparser from "js2xmlparser";
+import * as xml2js from 'xml2js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ng-app';
+  xml = `
+  <foo:Foo xmlns:foo="http://foo.com">
+    <bar:Bar xmlns:bar="http://bar.com"/>
+  </foo:Foo>`;
+
+  obj = {
+    'foo:Foo': {
+      $: {
+        'xmlns:foo': 'http://foo.com',
+      },
+      'bar:Bar': {
+        $: {
+          'xmlns:bar': 'http://bar.com',
+        },
+      },
+    },
+  };
+
+  parsedXML: any;
+  parsedJSON: any;
+
+  parseXML() {
+    // this.parsedObj = js2xmlparser.parse("person", this.obj);
+    const builder = new xml2js.Builder();
+    this.parsedXML = builder.buildObject(this.obj);
+  }
+
+  parseJSON() {
+    xml2js.parseStringPromise(this.xml).then((data) => {
+      this.parsedJSON = data;
+    });
+  }
 }
